45e47c3a1b57a78426caf81b241cd7e4319b451d
fer braga <augustofbg@gmail.com>
2022-03-22 18:56:20 -0300

    requisito 3 ok

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import FavoritePokemons from '../components/FavoritePokemons';
+import renderWithRouter from './renderWithRouter';
+
+describe('3. Teste o componente FavoritePokemons', () => {
+  it('Teste se é exibido na tela a mensagem No favorite pokemon found.', () => {
+    renderWithRouter(<FavoritePokemons />);
+
+    const text = screen.getByText(/no favorite pokemon found/i);
+    expect(text).toBeInTheDocument();
+  });
+
+  it('Teste se é exibido todos os cards de pokémons favoritados.', () => {
+    renderWithRouter(<FavoritePokemons />);
+
+    const imagens = screen.getByRole('heading');
+    expect(imagens).toBeInTheDocument();
+  });
+});
